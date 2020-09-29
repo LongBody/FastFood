b7042988069c9c7d94f36b1726181ceeb4d84472
LongBody <54003924+LongBody@users.noreply.github.com>
2020-09-29 20:48:00 +0700

    update

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ app.use(function(req, res, next) {
     if (allowedOrigins.indexOf(origin) > -1) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
